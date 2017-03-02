fd9ad67d4671996061c7d5b582ea29c09dbea71b
Maggie29 <zmagali@yahoo.com>
2017-03-02 18:48:21 -0500

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+var datos =
+[{nombre : "Arabela", apellido : "Rojas", rol : "T.A", cumpleanios : "23 de enero" },
+ {nombre : "Michelle", apellido : "Seguil", rol : "T.A", cumpleanios : "25 de noviembre" },
+ {nombre : "Meche", apellido : "Zubieta", rol : "T.A", cumpleanios : "02 de marzo" },
+ { nombre : "Papu", apellido : "Rivarola", rol : "Psicóloga", cumpleanios : "10 de octubre" },
+ { nombre : "Gian", apellido : "Corzo", rol : "Profesor", cumpleanios : "23 de enero" }]
