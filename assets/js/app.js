9bfbbd3b04e5cbb62813eccd8e386a9976e9f7e9
Nicolas Rasmussen <Nicolas Rasmussen>
2017-03-26 21:32:13 -0500

    creandoDivs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,24 @@
 var personal = document.getElementById('personal');
 var datos =
-[{nombre : "Arabela", apellido : "Rojas", rol : "T.A", cumpleanios : "23 de enero" },
- {nombre : "Michelle", apellido : "Seguil", rol : "T.A", cumpleanios : "25 de noviembre" },
- {nombre : "Meche", apellido : "Zubieta", rol : "T.A", cumpleanios : "02 de marzo" },
- { nombre : "Papu", apellido : "Rivarola", rol : "Psicóloga", cumpleanios : "10 de octubre" },
- { nombre : "Gian", apellido : "Corzo", rol : "Profesor", cumpleanios : "23 de enero" }];
+[{Nombre : "Arabela", Apellido : "Rojas", Rol : "T.A", Cumpleanios : "23 de enero" },
+ {Nombre : "Michelle", Apellido : "Seguil", Rol : "T.A", Cumpleanios : "25 de noviembre" },
+ {Nombre : "Meche", Apellido : "Zubieta", Rol : "T.A", Cumpleanios : "02 de marzo" },
+ { Nombre : "Papu", Apellido : "Rivarola", Rol : "Psicóloga", Cumpleanios : "10 de octubre" },
+ { Nombre : "Gian", Apellido : "Corzo", Rol : "Profesor", Cumpleanios : "23 de enero" }];
+
+ datos.forEach(function(element,index,array){
+ 	var divPersonal = document.createElement("div");
+ 	for(var prop in element){
+     divPersonal.classList.add("div-personal");
+ 		var pContainer = document.createElement("p");
+ 		pContainer.innerHTML = "<ul>"+"<li>"+prop + ": " + element[prop]+"</li>" +"</ul>";
+ 		divPersonal.appendChild(pContainer);
+ 	}
+ 	personal.appendChild(divPersonal)
+ })
+
+ 
+/*
 var lista ="";
  datos.forEach(function(item){
           lista += "<div><ul>"+
@@ -16,3 +30,4 @@ var lista ="";
 
    });
   personal.innerHTML = lista;
+*/
